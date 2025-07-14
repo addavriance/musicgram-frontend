dbaa73878da478d3a990738ab9ed5d941cf67514
addavriance <61160742+addavriance@users.noreply.github.com>
2025-07-15 02:35:19 +0300

    feat: yet another gh pages routing workaround

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { HashRouter as Router, Routes, Route } from 'react-router-dom'
 import LandingPage from './components/LandingPage'
 import AuthCallback from './components/AuthCallback'
 import ErrorPage from './components/ErrorPage'
@@ -6,7 +6,7 @@ import './styles/main.css'
 
 function App() {
     return (
-        <Router basename="/musicgram-frontend">
+        <Router basename="/">
             <div className="app">
                 <Routes>
                     <Route path="/" element={<LandingPage />} />
